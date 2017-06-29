934a6cdcad9f9e2cb99cf92cdef9aaa96a76ff9f
jaerbi <jaerbi42@gmail.com>
2017-06-29 15:48:24 +0000

    practic js03

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('button').click(handleClick); 
+})
+
+function handleClick() {
+    $('h1').html('Hello jQuery!');
+  //  $('button').unbind('click', handleClick);
+    $('button').click(handleSecondClick);
+}
+
+
+function handleSecondClick() {
+    $('h1').toggle('slow');
+}
+
+
+// var arr = document.querySelectorAll('h1');
+// arr.forEach(function(el) {
+//     el.innerHTML = 'Hello from Vanilla JS'
+// })
